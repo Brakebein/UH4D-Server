fe5d0566f5c716c318a4863d8c27a40ace0b57ae
Unknown <bruschie@hotmail.com>
2019-03-14 17:52:49 +0100

    query objects by date, new response format
    update dates of object

--- cyphers/model.cypher
+++ cyphers/model.cypher
@@ -24,7 +24,7 @@ CREATE (dobj)-[:P2 {order: i}]->(mat)
 RETURN DISTINCT dobj, file, collect(mat) AS materials;
 
 // query models
-MATCH (event:D7:UH4D {id: "d7_Hy0RgTjMf"})-[:L11]->(dobj:D1)-[:P106]->(file:D9)
+MATCH (event:D7:UH4D {id: 'd7_Hy0RgTjMf'})-[:L11]->(dobj:D1)-[:P106]->(file:D9)
 OPTIONAL MATCH (dobj)-[rmat:P2]->(mat:E57)
 WITH event, dobj, file, mat
 ORDER BY rmat.order
@@ -37,11 +37,37 @@ RETURN dobj.id AS id,
        materials,
        parent.id AS parent;
 
+// query models (new query)
+MATCH (e22:E22:UH4D {id: $id})-[:P1]->(name:E41)
+MATCH (e22)-[:P67|P106*1..]-(obj:D1)
+MATCH (obj)<-[:L11]-(event:D7),
+      (obj)-[:P106]->(file:D9)
+
+OPTIONAL MATCH (e22)<-[:P108]-(:E12)-[:P4]->(:E52)-[:P82]->(from:E61)
+OPTIONAL MATCH (e22)<-[:P13]-(:E6)-[:P4]->(:E52)-[:P82]->(to:E61)
+WITH e22, event, obj, file, name, {from: toString(from.value), to: toString(to.value)} AS date
+
+OPTIONAL MATCH (obj)-[rmat:P2]->(mat:E57)
+WITH e22, event, obj, file, name, date, mat
+  ORDER BY rmat.order
+WITH e22, event, obj, file, name, date, collect(mat) AS materials
+OPTIONAL MATCH (obj)<-[:P106]-(parent:D1)
+RETURN e22.id AS id,
+       name.value AS name,
+       date,
+       collect({id:        obj.id,
+        eventId:   event.id,
+        obj:       obj,
+        file:      file,
+        materials: materials,
+        parent:    parent.id
+       }) AS object;
+
 
 // merge E22 and E41 node
 MATCH (obj:D1:UH4D)
 WHERE NOT (obj)<-[:P106]-(:D1)
 MERGE (obj)-[:P67]->(e22:E22:UH4D)
-ON CREATE SET e22.id = "e22_" + obj.id
+ON CREATE SET e22.id = 'e22_' + obj.id
 MERGE (e22)-[:P1]->(e41:E41:UH4D)
-ON CREATE SET e41.id = "e41_" + obj.id, e41.value = obj.name
+ON CREATE SET e41.id = 'e41_' + obj.id, e41.value = obj.name
--- routes/digitalobject.js
+++ routes/digitalobject.js
@@ -1,29 +1,99 @@
-const utils = require('../modules/utils');
-const neo4j = require('../modules/neo4j-request');
+const utils = require('../modules/utils'),
+	neo4j = require('../modules/neo4j-request'),
+	shortid = require('shortid'),
+	moment = require('moment');
+
+
+function createHierarchy(data) {
+	for (let i = 0; i < data.length; i++) {
+		let obj = data[i];
+
+		if (!obj.children) obj.children = [];
+		if (obj.parent) {
+			let parent = getObjectById(data, obj.parent);
+			if (parent) {
+				if (!parent.children) parent.children = [];
+				parent.children.push(obj);
+				data.splice(i, 1);
+				i--;
+			}
+		}
+	}
+
+	return data;
+}
+
+function getObjectById(data, id) {
+	for (let i = 0; i < data.length; i++) {
+		if (data[i].id === id) return data[i];
+		if (data[i].children) {
+			let obj = getObjectById(data[i].children, id);
+			if (obj !== undefined) return obj;
+		}
+	}
+	return undefined;
+}
+
 
 module.exports = {
 
 	query: function (req, res) {
 
+		// check query parameter
+		let date = req.query.modelDate ? moment(req.query.modelDate) : null;
+		if (date && !date.isValid())
+			date = null;
+
+		let params = {};
+
 		// language=Cypher
 		let q = `
-			MATCH (event:D7:UH4D)-[:L11]->(dobj:D1)-[:P106]->(file:D9),
-                  (dobj)-[:P67]->(:E22)-[:P1]->(name:E41)
-			OPTIONAL MATCH (dobj)-[rmat:P2]->(mat:E57)
-			WITH event, dobj, file, name, mat
-			ORDER BY rmat.order
-			WITH event, dobj, file, name, collect(mat) AS materials
-			OPTIONAL MATCH (dobj)<-[:P106]-(parent:D1)
-			RETURN dobj.id AS id,
+			MATCH (e22:E22:UH4D)-[:P1]->(name:E41)
+			MATCH (e22)-[:P67|P106*1..]-(obj:D1)
+			MATCH (obj)<-[:L11]-(event:D7),
+				  (obj)-[:P106]->(file:D9)
+			
+			OPTIONAL MATCH (e22)<-[:P108]-(:E12)-[:P4]->(:E52)-[:P82]->(from:E61)
+			OPTIONAL MATCH (e22)<-[:P13]-(:E6)-[:P4]->(:E52)-[:P82]->(to:E61)`;
+
+		if (date) {
+			q += `
+			WITH e22, event, obj, file, name, from, to
+            WHERE (from IS NULL OR from.value < date($date)) AND (to IS NULL OR to.value > date($date))`;
+
+			params.date = date.format('YYYY-MM-DD');
+		}
+
+		// language=Cypher
+		q += `
+			WITH e22, event, obj, file, name, {from: toString(from.value), to: toString(to.value)} AS date
+			
+			OPTIONAL MATCH (obj)-[rmat:P2]->(mat:E57)
+			WITH e22, event, obj, file, name, date, mat
+			  ORDER BY rmat.order
+			WITH e22, event, obj, file, name, date, collect(mat) AS materials
+			OPTIONAL MATCH (obj)<-[:P106]-(parent:D1)
+			RETURN e22.id AS id,
 				   name.value AS name,
-				   event.id AS eventId,
-				   dobj AS obj,
-				   file AS file,
-				   materials,
-				   parent.id AS parent`;
+				   date,
+				   collect({id: obj.id,
+					eventId:   event.id,
+					obj:       obj,
+					file:      file,
+					materials: materials,
+					parent:    parent.id
+				   }) AS object;`;
+
 		
-		neo4j.readTransaction(q)
+		neo4j.readTransaction(q, params)
 			.then(function (results) {
+				results.forEach(function (value) {
+					createHierarchy(value.object);
+					if (value.object.length === 1)
+						value.object = value.object[0];
+					else
+						console.warn('No single root object', value.object);
+				});
 				res.json(results);
 			})
 			.catch(function (err) {
@@ -35,29 +105,46 @@ module.exports = {
 
 		// language=Cypher
 		let q = `
-			MATCH (dobj:D1:UH4D {id: $objectId})<-[:L11]-(event:D7),
-				(dobj)-[:P106]->(file:D9),
-				(dobj)-[:P67]->(:E22)-[:P1]->(name:E41)
-			OPTIONAL MATCH (dobj)-[rmat:P2]->(mat:E57)
-			WITH event, dobj, file, name, mat
-			ORDER BY rmat.order
-			WITH event, dobj, file, name, collect(mat) AS materials
-			OPTIONAL MATCH (dobj)<-[:P106]-(parent:D1)
-			RETURN dobj.id AS id,
+			MATCH (e22:E22:UH4D {id: $id})-[:P1]->(name:E41)
+			MATCH (e22)-[:P67|P106*1..]-(obj:D1)
+			MATCH (obj)<-[:L11]-(event:D7),
+				  (obj)-[:P106]->(file:D9)
+			
+			OPTIONAL MATCH (e22)<-[:P108]-(:E12)-[:P4]->(:E52)-[:P82]->(from:E61)
+			OPTIONAL MATCH (e22)<-[:P13]-(:E6)-[:P4]->(:E52)-[:P82]->(to:E61)
+			WITH e22, event, obj, file, name, {from: toString(from.value), to: toString(to.value)} AS date
+			
+			OPTIONAL MATCH (obj)-[rmat:P2]->(mat:E57)
+			WITH e22, event, obj, file, name, date, mat
+			  ORDER BY rmat.order
+			WITH e22, event, obj, file, name, date, collect(mat) AS materials
+			OPTIONAL MATCH (obj)<-[:P106]-(parent:D1)
+			RETURN e22.id AS id,
 				   name.value AS name,
-				   event.id AS eventId,
-				   dobj AS obj,
-				   file AS file,
-				   materials,
-				   parent.id AS parent`;
+				   date,
+				   collect({id:        obj.id,
+					eventId:   event.id,
+					obj:       obj,
+					file:      file,
+					materials: materials,
+					parent:    parent.id
+				   }) AS object;`;
 
 		let params = {
-			objectId: req.params.id
+			id: req.params.id
 		};
 
 		neo4j.readTransaction(q, params)
 			.then(function (results) {
-				res.json(results[0]);
+				let value = results[0];
+				if (value) {
+					createHierarchy(value.object);
+					if (value.object.length === 1)
+						value.object = value.object[0];
+					else
+						console.warn('No single root object', value.object);
+				}
+				res.json(value);
 			})
 			.catch(function (err) {
 				utils.error.neo4j(res, err, '#digitalobject.get');
@@ -66,20 +153,50 @@ module.exports = {
 
 	update: function (req, res) {
 
-		let q = `MATCH (dobj:D1:UH4D {id: $id})-[:P67]-(e22:E22) `;
+		let id = shortid.generate();
+
+		let q = `MATCH (e22:E22:UH4D {id: $id})<-[:P67]-(obj:D1) `;
 		let params = {
 			id: req.params.id
 		};
 
 		switch (req.query.prop) {
 			case 'name':
-				q += `MATCH (dobj)-[:P1]->(name:E41)
+				q += `MATCH (e22)-[:P1]->(name:E41)
 					SET name.value = $name`;
 				params.name = req.body.name;
 				break;
+			case 'from':
+				let fromDate = moment(req.body.date.from);
+				if (!fromDate.isValid() || (req.body.date.to && moment(req.body.date.to).isAfter(fromDate))) {
+					res.json();
+					return;
+				}
+				q += `
+					MERGE (e22)<-[:P108]-(e12:E12:UH4D)-[:P4]->(e52:E52:UH4D)-[:P82]->(date:E61:UH4D)
+					ON CREATE SET e12.id = $e12id, e52.id = $e52id
+					SET date.value = date($date)`;
+				params.e12id = 'e12_' + id;
+				params.e52id = 'e52_e12_' + id;
+				params.date = fromDate.format('YYYY-MM-DD');
+				break;
+			case 'to':
+				let toDate = moment(req.body.date.to);
+				if (!toDate.isValid() || (req.body.date.from && moment(req.body.date.from).isBefore(toDate))) {
+					res.json();
+					return;
+				}
+				q += `
+					MERGE (e22)<-[:P13]-(e6:E6:UH4D)-[:P4]->(e52:E52:UH4D)-[:P82]->(date:E61:UH4D)
+					ON CREATE SET e6.id = $e6id, e52.id = $e52id
+					SET date.value = date($date)`;
+				params.e6id = 'e6_' + id;
+				params.e52id = 'e52_e6_' + id;
+				params.date = toDate.format('YYYY-MM-DD');
+				break;
 		}
 
-		q += ' RETURN dobj';
+		q += ' RETURN obj';
 
 		neo4j.readTransaction(q, params)
 			.then(function () {
